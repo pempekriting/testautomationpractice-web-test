00a65e376232f5221076f0fe2471baff03716b11
Kgs Azzam Nizar <kgs.azzam@gmail.com>
2025-06-20 04:26:43 +0700

    Add steps to take screenshots

--- features/support/hooks.ts
+++ features/support/hooks.ts
@@ -1,10 +1,14 @@
-import { Before, After } from '@cucumber/cucumber';
+import { Before, After, Status } from '@cucumber/cucumber';
 import { CustomWorld } from './world';
 
 Before(async function (this: CustomWorld) {
   await this.init();
 });
 
-After(async function (this: CustomWorld) {
+After(async function (this: CustomWorld, scenario) {
+  if(scenario.result?.status === Status.PASSED || scenario.result?.status === Status.FAILED){
+    const bufferPict = await this.page.screenshot();
+    this.attach(bufferPict, `image/png`);
+  }
   await this.close();
 });
